d7d876c2eefbe2e9c5044a991e79de8592f5f750
Benjamin Wollenweber <ich@wollis-page.de>
2024-01-06 14:26:58 +0100

    Add check file exists on pdf processes

--- src/Service/PdfService.php
+++ src/Service/PdfService.php
@@ -5,12 +5,15 @@ namespace GibsonOS\Module\Obscura\Service;
 
 use GibsonOS\Core\Attribute\GetEnv;
 use GibsonOS\Core\Exception\ProcessError;
+use GibsonOS\Core\Service\FileService;
 use GibsonOS\Core\Service\ProcessService;
+use GibsonOS\Module\Obscura\Exception\PdfException;
 
 class PdfService
 {
     public function __construct(
         private readonly ProcessService $processService,
+        private readonly FileService $fileServices,
         #[GetEnv('TIFF2PDF_PATH')]
         private readonly string $tiff2PdfPath,
         #[GetEnv('OCRMYPDF_PATH')]
@@ -22,35 +25,72 @@ class PdfService
 
     /**
      * @throws ProcessError
+     * @throws PdfException
      */
     public function tiff2pdf(string $tiffFilename, string $pdfFilename): void
     {
+        if (!$this->fileServices->exists($tiffFilename)) {
+            throw new PdfException(sprintf('TIFF file "%s" does not exists!', $tiffFilename));
+        }
+
+        if ($this->fileServices->exists($pdfFilename)) {
+            throw new PdfException(sprintf('PDF file "%s" already exists!', $pdfFilename));
+        }
+
         $this->processService->execute(sprintf(
             '%s -o %s %s',
             $this->tiff2PdfPath,
             escapeshellarg($pdfFilename),
             escapeshellarg($tiffFilename),
         ));
+
+        if (!$this->fileServices->exists($pdfFilename)) {
+            throw new PdfException(sprintf('PDF file "%s" not created!', $pdfFilename));
+        }
     }
 
     /**
      * @throws ProcessError
+     * @throws PdfException
      */
     public function ocrPdf(string $inputFilename, string $outputFilename): void
     {
+        if (!$this->fileServices->exists($inputFilename)) {
+            throw new PdfException(sprintf('OCR input PDF file "%s" does not exists!', $inputFilename));
+        }
+
+        if ($this->fileServices->exists($outputFilename)) {
+            throw new PdfException(sprintf('OCR PDF file "%s" already exists!', $outputFilename));
+        }
+
         $this->processService->execute(sprintf(
             '%s %s %s -l deu+eng --image-dpi 300 --deskew --clean --rotate-pages',
             $this->ocrMyPdfPath,
             escapeshellarg($inputFilename),
             escapeshellarg($outputFilename),
         ));
+
+        if (!$this->fileServices->exists($outputFilename)) {
+            throw new PdfException(sprintf('OCR PDF file "%s" not created!', $outputFilename));
+        }
     }
 
     /**
      * @throws ProcessError
+     * @throws PdfException
      */
     public function pdfUnite(array $pdfFilenames, string $filename): void
     {
+        foreach ($pdfFilenames as $pdfFilename) {
+            if (!$this->fileServices->exists($pdfFilename)) {
+                throw new PdfException(sprintf('Input PDF file "%s" does not exists!', $pdfFilename));
+            }
+        }
+
+        if ($this->fileServices->exists($filename)) {
+            throw new PdfException(sprintf('Merged PDF file "%s" already exists!', $filename));
+        }
+
         $this->processService->execute(sprintf(
             '%s %s %s',
             $this->pdfUnitePath,
@@ -63,5 +103,9 @@ class PdfService
             ),
             escapeshellarg($filename),
         ));
+
+        if (!$this->fileServices->exists($filename)) {
+            throw new PdfException(sprintf('Merged PDF file "%s" does not exists!', $filename));
+        }
     }
 }
--- tests/Unit/Service/PdfServiceTest.php
+++ tests/Unit/Service/PdfServiceTest.php
@@ -4,7 +4,9 @@ declare(strict_types=1);
 namespace GibsonOS\Test\Unit\Obscura\Service;
 
 use Codeception\Test\Unit;
+use GibsonOS\Core\Service\FileService;
 use GibsonOS\Core\Service\ProcessService;
+use GibsonOS\Module\Obscura\Exception\PdfException;
 use GibsonOS\Module\Obscura\Service\PdfService;
 use Prophecy\PhpUnit\ProphecyTrait;
 use Prophecy\Prophecy\ObjectProphecy;
@@ -17,20 +19,32 @@ class PdfServiceTest extends Unit
 
     private ProcessService|ObjectProphecy $processService;
 
+    private FileService|ObjectProphecy $fileService;
+
     protected function _before()
     {
         $this->processService = $this->prophesize(ProcessService::class);
+        $this->fileService = $this->prophesize(FileService::class);
 
         $this->pdfService = new PdfService(
             $this->processService->reveal(),
+            $this->fileService->reveal(),
             'tiff2pdf',
             'ocrMyPdf',
             'pdfUnite',
         );
     }
 
-    public function testTiff2Pdf(): void
+    public function testTiff2PdfSuccess(): void
     {
+        $this->fileService->exists('dent.tiff')
+            ->shouldBeCalledOnce()
+            ->willReturn(true)
+        ;
+        $this->fileService->exists('arthur.pdf')
+            ->shouldBeCalledTimes(2)
+            ->willReturn(false, true)
+        ;
         $this->processService->execute('tiff2pdf -o "arthur.pdf" "dent.tiff"')
             ->shouldBeCalledOnce()
             ->willReturn('')
@@ -39,8 +53,64 @@ class PdfServiceTest extends Unit
         $this->pdfService->tiff2pdf('dent.tiff', 'arthur.pdf');
     }
 
-    public function testOcrPdf(): void
+    public function testTiff2PdfNotCreated(): void
     {
+        $this->fileService->exists('dent.tiff')
+            ->shouldBeCalledOnce()
+            ->willReturn(true)
+        ;
+        $this->fileService->exists('arthur.pdf')
+            ->shouldBeCalledTimes(2)
+            ->willReturn(false, false)
+        ;
+        $this->processService->execute('tiff2pdf -o "arthur.pdf" "dent.tiff"')
+            ->shouldBeCalledOnce()
+            ->willReturn('')
+        ;
+
+        $this->expectException(PdfException::class);
+
+        $this->pdfService->tiff2pdf('dent.tiff', 'arthur.pdf');
+    }
+
+    public function testTiff2PdfAlreadyExists(): void
+    {
+        $this->fileService->exists('dent.tiff')
+            ->shouldBeCalledOnce()
+            ->willReturn(true)
+        ;
+        $this->fileService->exists('arthur.pdf')
+            ->shouldBeCalledOnce()
+            ->willReturn(true)
+        ;
+
+        $this->expectException(PdfException::class);
+
+        $this->pdfService->tiff2pdf('dent.tiff', 'arthur.pdf');
+    }
+
+    public function testTiff2PdfNotFound(): void
+    {
+        $this->fileService->exists('dent.tiff')
+            ->shouldBeCalledOnce()
+            ->willReturn(false)
+        ;
+
+        $this->expectException(PdfException::class);
+
+        $this->pdfService->tiff2pdf('dent.tiff', 'arthur.pdf');
+    }
+
+    public function testOcrPdfSuccess(): void
+    {
+        $this->fileService->exists('arthur.pdf')
+            ->shouldBeCalledOnce()
+            ->willReturn(true)
+        ;
+        $this->fileService->exists('dent.pdf')
+            ->shouldBeCalledTimes(2)
+            ->willReturn(false, true)
+        ;
         $this->processService->execute('ocrMyPdf "arthur.pdf" "dent.pdf" -l deu+eng --image-dpi 300 --deskew --clean --rotate-pages')
             ->shouldBeCalledOnce()
             ->willReturn('')
@@ -49,12 +119,144 @@ class PdfServiceTest extends Unit
         $this->pdfService->ocrPdf('arthur.pdf', 'dent.pdf');
     }
 
-    public function testPdfUnite(): void
+    public function testOcrPdfNotCreated(): void
     {
+        $this->fileService->exists('arthur.pdf')
+            ->shouldBeCalledOnce()
+            ->willReturn(true)
+        ;
+        $this->fileService->exists('dent.pdf')
+            ->shouldBeCalledTimes(2)
+            ->willReturn(false, false)
+        ;
+        $this->processService->execute('ocrMyPdf "arthur.pdf" "dent.pdf" -l deu+eng --image-dpi 300 --deskew --clean --rotate-pages')
+            ->shouldBeCalledOnce()
+            ->willReturn('')
+        ;
+
+        $this->expectException(PdfException::class);
+
+        $this->pdfService->ocrPdf('arthur.pdf', 'dent.pdf');
+    }
+
+    public function testOcrPdfAlreadyExists(): void
+    {
+        $this->fileService->exists('arthur.pdf')
+            ->shouldBeCalledOnce()
+            ->willReturn(true)
+        ;
+        $this->fileService->exists('dent.pdf')
+            ->shouldBeCalledOnce()
+            ->willReturn(true)
+        ;
+
+        $this->expectException(PdfException::class);
+
+        $this->pdfService->ocrPdf('arthur.pdf', 'dent.pdf');
+    }
+
+    public function testOcrPdfNotFound(): void
+    {
+        $this->fileService->exists('arthur.pdf')
+            ->shouldBeCalledOnce()
+            ->willReturn(false)
+        ;
+
+        $this->expectException(PdfException::class);
+
+        $this->pdfService->ocrPdf('arthur.pdf', 'dent.pdf');
+    }
+
+    public function testPdfUniteSuccess(): void
+    {
+        $this->fileService->exists('arthur.pdf')
+            ->shouldBeCalledOnce()
+            ->willReturn(true)
+        ;
+        $this->fileService->exists('dent.pdf')
+            ->shouldBeCalledOnce()
+            ->willReturn(true)
+        ;
+        $this->fileService->exists('ford.pdf')
+            ->shouldBeCalledTimes(2)
+            ->willReturn(false, true)
+        ;
         $this->processService->execute('pdfUnite "arthur.pdf" "dent.pdf" "ford.pdf"')
             ->shouldBeCalledOnce()
             ->willReturn('')
         ;
+
+        $this->pdfService->pdfUnite(['arthur.pdf', 'dent.pdf'], 'ford.pdf');
+    }
+
+    public function testPdfUniteNotCreated(): void
+    {
+        $this->fileService->exists('arthur.pdf')
+            ->shouldBeCalledOnce()
+            ->willReturn(true)
+        ;
+        $this->fileService->exists('dent.pdf')
+            ->shouldBeCalledOnce()
+            ->willReturn(true)
+        ;
+        $this->fileService->exists('ford.pdf')
+            ->shouldBeCalledTimes(2)
+            ->willReturn(false, false)
+        ;
+        $this->processService->execute('pdfUnite "arthur.pdf" "dent.pdf" "ford.pdf"')
+            ->shouldBeCalledOnce()
+            ->willReturn('')
+        ;
+
+        $this->expectException(PdfException::class);
+
+        $this->pdfService->pdfUnite(['arthur.pdf', 'dent.pdf'], 'ford.pdf');
+    }
+
+    public function testPdfUniteAlreadyExists(): void
+    {
+        $this->fileService->exists('arthur.pdf')
+            ->shouldBeCalledOnce()
+            ->willReturn(true)
+        ;
+        $this->fileService->exists('dent.pdf')
+            ->shouldBeCalledOnce()
+            ->willReturn(true)
+        ;
+        $this->fileService->exists('ford.pdf')
+            ->shouldBeCalledOnce()
+            ->willReturn(true)
+        ;
+
+        $this->expectException(PdfException::class);
+
+        $this->pdfService->pdfUnite(['arthur.pdf', 'dent.pdf'], 'ford.pdf');
+    }
+
+    public function testPdfUniteFile2NotFound(): void
+    {
+        $this->fileService->exists('arthur.pdf')
+            ->shouldBeCalledOnce()
+            ->willReturn(true)
+        ;
+        $this->fileService->exists('dent.pdf')
+            ->shouldBeCalledOnce()
+            ->willReturn(false)
+        ;
+
+        $this->expectException(PdfException::class);
+
+        $this->pdfService->pdfUnite(['arthur.pdf', 'dent.pdf'], 'ford.pdf');
+    }
+
+    public function testPdfUniteFile1NotFound(): void
+    {
+        $this->fileService->exists('arthur.pdf')
+            ->shouldBeCalledOnce()
+            ->willReturn(false)
+        ;
+
+        $this->expectException(PdfException::class);
 
         $this->pdfService->pdfUnite(['arthur.pdf', 'dent.pdf'], 'ford.pdf');
     }
